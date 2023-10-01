d640857a007d50af2d4c165748833b01d088d7f2
KarinaVeremeyeva <57318127+KarinaVeremeyeva@users.noreply.github.com>
2023-10-01 22:29:47 +0300

    Add solved task

---
+++ TestTask/Services/OrderService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services
+{
+    public class OrderService : IOrderService
+    {
+        private ApplicationDbContext _context;
+
+        public OrderService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Order> GetOrder()
+        {
+            var maxOrderAmount = await _context.Orders
+                .OrderByDescending(x => x.Price * x.Quantity)
+                .FirstOrDefaultAsync();
+
+            return maxOrderAmount;
+        }
+
+        public async Task<List<Order>> GetOrders()
+        {
+            var filteredOrders = await _context.Orders
+                .Where(x => x.Quantity > 10)
+                .ToListAsync();
+
+            return filteredOrders;
+        }
+    }
+}
---
+++ TestTask/Services/UserService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services
+{
+    public class UserService : IUserService
+    {
+        private ApplicationDbContext _context;
+
+        public UserService(ApplicationDbContext applicationDbContext)
+        {
+            _context = applicationDbContext;
+        }
+
+        public async Task<User> GetUser()
+        {
+            var user = await _context.Users
+                .Include(x => x.Orders)
+                .OrderByDescending(x => x.Orders.Count)
+                .FirstOrDefaultAsync();
+
+            return user;
+        }
+
+        public async Task<List<User>> GetUsers()
+        {
+            var inactiveUsers = await _context.Users
+                .Where(x => x.Status == Enums.UserStatus.Inactive)
+                .ToListAsync();
+
+            return inactiveUsers;
+        }
+    }
+}
